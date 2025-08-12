213fdc577ee3aadb61c67587825178584e985599
amansingh760 <amansingh8137@gmail.com>
2025-08-12 07:27:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require('express');
+// const mongoose = require('mongoose');
+const { MongoClient, ObjectId } = require('mongodb');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv');
+const cors = require('cors');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+dotenv.config();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
+
+// MongoDB connection
+let db;
+MongoClient.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to MongoDB');
+        db = client.db(); // Set the database connection
+    })
+    .catch(err => console.log(err));
+
+// Define a basic route
+app.get('/', (req, res) => {
+    res.send('Hello, this is your API!');
+});
+
+app.get('/state-data', async (req, res) => {
+    try {
+        const state_data = await db.collection('all').distinct('state');
+        res.json(state_data);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch users' });
+    }
+});
+
+app.post('/get-museum-data', async (req, res) => {
+    try {
+        const result = await db.collection('all').find({ state: req.body.state }).toArray();
+        res.status(201).json(result); // Return the created user
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to create user' });
+    }
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
